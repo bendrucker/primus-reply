ad0a3677d7963317fd796d3e66aae88b28e0b740
Ben Drucker <bvdrucker@gmail.com>
2014-01-27 12:00:29 -0500

    Exclude client plugin from root plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,3 @@
 'use strict';
 
-var server = require('./server/plugin').server,
-    client = require('./client/plugin').client,
-    fs     = require('fs');
-
-client.toString = function () {
-  return require('../build/client');
-};
-
-module.exports = {
-  server: server,
-  client: client
-};
+module.exports = require('./server/plugin');
