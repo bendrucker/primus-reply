b1f5dc480b1d4d9160d33a4fb7cf854a7ea1e895
Ben Drucker <bvdrucker@gmail.com>
2014-01-26 20:26:43 -0500

    Run tests from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 'use strict';
 
 var gulp       = require('gulp'),
+    util       = require('gulp-util'),
     browserify = require('gulp-browserify'),
     rename     = require('gulp-rename'),
-    size       = require('gulp-filesize');
+    size       = require('gulp-filesize'),
+    mocha      = require('gulp-mocha'),
+    spawn      = require('child_process').spawn,
+    path       = require('path');
 
 gulp.task('build', function () {
   gulp.src('src/client/plugin.js', {read: false})
@@ -13,4 +17,18 @@ gulp.task('build', function () {
     .pipe(rename('primus-reply-client.js'))
     .pipe(gulp.dest('./build'))
     .pipe(size());
+});
+
+gulp.task('test:unit', function () {
+  require('./test/setup');
+  gulp.src('test/unit/**/*.js')
+    .pipe(mocha())
+});
+
+gulp.task('test:integration', function (done) {
+  var child = spawn('node', [
+    path.join(__dirname, 'test', 'integration', 'runner.js'),
+  ], {
+    stdio: 'inherit'
+  }).on('exit', done);
 });
